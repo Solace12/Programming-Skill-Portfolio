3b98aeba859d10bcdb1640ec6d9096350435620c
Solace12 <165131002+Solace12@users.noreply.github.com>
2024-10-30 13:59:02 +0400

    Delete Exercise 6.py

--- Exercise 6.py
+++
@@ -1,9 +0,0 @@
-CorrectPassword ="12345"
-password = ""
-
-while password != CorrectPassword:
-    password = input("Enter Password: ")
-    if password != CorrectPassword:
-        print("Incorrect password Please try again!")
-    else:
-        print("The password you entered is Correct!")
